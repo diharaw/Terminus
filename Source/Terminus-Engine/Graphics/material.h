c668aac81fca43006d369290d237bb9df7fe67f8
Dihara Wijetunga <shredhead94@gmail.com>
2017-01-17 12:47:58 +0530

    - moar renaming

---
+++ Source/Terminus-Engine/Graphics/material.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <types.h>
+#include <Graphics/render_device.h>
+
+// Material Key Options
+
+// Normal Mapping
+// Parallax Occlusion
+// Diffuse Maps or Constant color
+// Roughness Maps or value
+// Metalness Maps or value
+
+namespace terminus
+{
+	// Forward Declaration
+	struct Texture2D;
+
+	enum TexureMap
+	{
+		DIFFUSE = 0,
+		NORMAL = 1,
+		ROUGHNESS = 2,
+		METALNESS = 3,
+		DISPLACEMENT = 4
+	};
+    
+    enum class TessellationType
+    {
+        OFF = 0,
+        PN_TRIANGLES
+    };
+
+	struct Material
+	{
+		Texture2D*       texture_maps[5];
+        SamplerState*    sampler;
+		bool             backface_cull;
+        TessellationType tessellation;
+		Vector4          diffuse_value;
+		float            roughness_value;
+		float            metalness_value;
+	};
+} // namespace terminus
---
+++ Source/Terminus-Engine/terminus.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#ifndef  TERMINUS_H
+#define TERMINUS_H
+
+#include <Core/application.h>
+
+#endif
