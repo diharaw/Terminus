706b71e2701abca22969b42c3e38d400e29cf9e8
Dihara Wijetunga <shredhead94@gmail.com>
2017-02-06 08:43:05 +0530

    - Updated Lua Test script.

--- data/assets/script/lua/test_lua_script.lua
+++ data/assets/script/lua/test_lua_script.lua
@@ -8,21 +8,34 @@ TestLuaScript = ScriptComponent:new
 		nNum = 1,
 		fSomething = 5.3,
 		bThing = false,
-		bAnotherThing = true
+		bAnotherThing = true,
+		listenerId
 	}
 }
 
+function TestLuaScript:OnSceneLoadComplete(event)
+	log_info("Scene load complete notification")
+end
+
 function TestLuaScript:initialize()
-	T_LOG_INFO("initialize from TestLuaScript")
+	log_info("initialize from TestLuaScript")
+
+	v1 = Vector3:new(3, 4, 5)
+	v2 = Vector3:new(1, 2, 3)
+	v3 = math.cross(v1, v2)
+
+	log_info("v3 = (" .. v3.x .. ",".. v3.y .. ",".. v3.z .. ")")
+
+	self.property.listenerId = event_handler.register_listener(0x8bd7b76e, self.OnSceneLoadComplete, self)
 end
 
 function TestLuaScript:update(dt)
 	if self.property.bAnotherThing == true then
-		T_LOG_INFO("update from TestLuaScript")
+		log_info("update from TestLuaScript")
 		self.property.bAnotherThing = false
 	end
 end
 
 function TestLuaScript:shutdown()
-	T_LOG_INFO("shutdown")
+	log_info("shutdown")
 end
