684542f21c747f6e6f21dfe3360e21a93a458142
Dihara Wijetunga <shredhead94@gmail.com>
2017-02-07 19:32:58 +0530

    - Updated assets.

--- data/assets/script/lua/test_lua_script.lua
+++ data/assets/script/lua/test_lua_script.lua
@@ -9,35 +9,52 @@ TestLuaScript = ScriptComponent:new
 		fSomething = 5.3,
 		bThing = false,
 		bAnotherThing = true,
-		listenerId
-	}
+		listenerId,
+		actionListenerId,
+		axisListenerId
+	},
+	firstNew = true,
+	default_player,
+	test_input_map
 }
 
 function TestLuaScript:OnSceneLoadComplete(event)
 	log_info("Scene load complete notification")
 end
 
+function TestLuaScript:OnActionInput(event)
+	log_info("Action input recieved!")
+end
+
+function TestLuaScript:OnAxisInput(event)
+	log_info("Axis input recieved!")
+end
+
 function TestLuaScript:initialize()
 	log_info("initialize from TestLuaScript")
+	self.property.listenerId = event_handler.register_listener(EventType.SCENE_LOAD, self.OnSceneLoadComplete, self)
+	self.property.actionListenerId = event_handler.register_listener(EventType.INPUT_ACTION, self.OnActionInput, self)
+	self.property.axisListenerId = event_handler.register_listener(EventType.INPUT_AXIS, self.OnAxisInput, self)
 
-	v1 = Vector3:new(3, 4, 5)
-	v2 = Vector3:new(1, 2, 3)
-	v3 = math.cross(v1, v2)
-
-	log_info("v3 = (" .. v3.x .. ",".. v3.y .. ",".. v3.z .. ")")
-
-	self.property.listenerId = event_handler.register_listener(0x8bd7b76e, self.OnSceneLoadComplete, self)
+	self.default_player = Input.get_default_player_context()
+	log_info("loading input map")
+	self.test_input_map = Input.load_input_map(self.default_player, "test_input_map.json")
+	Input.set_active_input_map(self.default_player, self.test_input_map)
 end
 
 function TestLuaScript:update(dt)
-
 	test_cmp = self._scene:get_transform_component(self._entity)
 
-	look_at_pos = Vector3:new(0.0, 0.0, -100.0)
-	--new_rotation.y = new_rotation.y + dt * 10.0
+	log_info("Lua Hot Reload!")
+	new_pos = test_cmp._position
+	new_pos.x = new_pos.x + dt * 20.0
 
-	--set_rotation_euler(test_cmp, new_rotation)
-	transform.look_at(test_cmp, look_at_pos)
+	transform.set_position(test_cmp, Vector3:new(1.0, 1.0, 1.0))
+	camera_cmp = self._scene:get_camera_component(self._entity)
+
+	cam_speed = dt * 10.0
+
+	camera.set_position(camera_cmp, Vector3:new(0.0, 0.0, 10.0))
 
 	if self.property.bAnotherThing == true then
 		log_info("update from TestLuaScript")
