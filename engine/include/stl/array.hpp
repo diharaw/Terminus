282854494a824a8cfe1a7fc172ba94be23a3d138
diharaw <shredhead94@gmail.com>
2018-05-08 22:36:50 +0530

    Added STL source to CMakeLists.

--- engine/include/stl/array.hpp
+++ engine/include/stl/array.hpp
@@ -41,6 +41,79 @@ public:
     {
         return m_elements[idx];
     }
+
+	class Iterator
+	{
+		T*	   data;
+		size_t	   position;
+		size_t num_elements;
+	public:
+		Iterator(T* _data, size_t _position, size_t _num_elements) : data(_data), position(_position), num_elements(_num_elements)
+		{
+		}
+
+		T& operator*()
+		{
+			return data[position];
+		}
+
+		Iterator& operator++()
+		{
+			position++;
+			return *this;
+		}
+
+		bool operator!=(const Iterator& it) const
+		{
+			return position != it.position;
+		}
+	};
+
+	class ConstIterator
+	{
+		T*		   data;
+		size_t	   position;
+		size_t	   num_elements;
+	public:
+		ConstIterator(T* _data, size_t _position, size_t _num_elements) : data(_data), position(_position), num_elements(_num_elements)
+		{
+		}
+
+		const T& operator*() const
+		{
+			return data[position];
+		}
+
+		ConstIterator& operator++()
+		{
+			position++; return *this;
+		}
+
+		bool operator!=(const ConstIterator& it) const
+		{
+			return position != it.position;
+		}
+	};
+
+	Iterator begin()
+	{
+		return{ m_elements, 0, N };
+	}
+
+	Iterator end()
+	{
+		return{ m_elements, N, N };
+	}
+
+	ConstIterator begin() const
+	{
+		{ m_elements, 0, N };
+	}
+
+	ConstIterator end() const
+	{
+		return{ m_elements, N, N };
+	}
     
 private:
     T m_elements[N];
