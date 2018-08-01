c49a46f1429ebe9aa8c7ede9568380dc5b665243
diharaw <shredhead94@gmail.com>
2018-08-01 22:46:13 +0530

    Direct3D 12 descriptor heap manager.

---
+++ engine/include/gfx/direct3d12/descriptor_heap_manager.hpp
@@ -0,0 +1,24 @@
+
+struct DescriptorHeapAlloc
+{
+    size_t                        alloc_idx;
+    CD3DX12_GPU_DESCRIPTOR_HANDLE gpu_handle;
+    CD3DX12_CPU_DESCRIPTOR_HANDLE cpu_handle;
+};
+
+class DescriptorHeapManager
+{
+public:
+    bool initialize(ID3D12Device* device, UINT descriptor_count, D3D12_DESCRIPTOR_HEAP_TYPE type, D3D12_DESCRIPTOR_HEAP_FLAG flags);
+    DescriptorHeapAlloc allocate_dynamic(uint32_t count);
+
+private:
+    UINT                          m_descriptor_size;
+    UINT                          m_total;
+    UINT                          m_allocated;
+    CD3DX12_CPU_DESCRIPTOR_HANDLE m_cpu_root_handle;
+    CD3DX12_CPU_DESCRIPTOR_HANDLE m_cpu_current_handle;
+    CD3DX12_GPU_DESCRIPTOR_HANDLE m_gpu_root_handle;
+    CD3DX12_GPU_DESCRIPTOR_HANDLE m_gpu_current_handle;
+    ComPtr<ID3D12DescriptorHeap>  m_heap;
+};
---
+++ engine/src/gfx/direct3d12/descriptor_heap_manager.cpp
@@ -0,0 +1,35 @@
+
+bool DescriptorHeapManager::initialize(ID3D12Device* device, UINT descriptor_count, D3D12_DESCRIPTOR_HEAP_TYPE type, D3D12_DESCRIPTOR_HEAP_FLAG flags)
+{
+    D3D12_DESCRIPTOR_HEAP_DESC heap_desc = {};
+	heap_desc.NumDescriptors = descriptor_count;
+	heap_desc.Type = type;
+	heap_desc.Flags = flags;
+	ThrowIfFailed(device->CreateDescriptorHeap(&heap_desc, IID_PPV_ARGS(&m_heap)));
+    m_descriptor_size = device->GetDescriptorHandleIncrementSize(type);
+    m_cpu_root_handle = m_heap->GetCPUDescriptorHandleForHeapStart();
+    m_gpu_root_handle = m_heap->GetGPUDescriptorHandleForHeapStart();
+    m_cpu_current_handle = m_cpu_root_handle;
+    m_gpu_current_handle = m_gpu_root_handle;
+    m_total = descriptor_count;
+    m_allocated = 0;
+}
+DescriptorHeapAlloc DescriptorHeapManager::allocate_dynamic(uint32_t count)
+{
+    m_allocated += count;
+    if (m_allocated > m_total)
+    {
+        // Reset current handle to the beginning of heap.
+        m_allocated = 0;
+        m_cpu_current_handle = m_cpu_root_handle;
+        m_gpu_current_handle = m_gpu_root_handle;
+    }
+    DescriptorHeapAlloc alloc;
+    alloc.alloc_idx = -1;
+    alloc.cpu_handle = m_cpu_current_handle;
+    alloc.gpu_handle = m_gpu_current_handle;
+    m_cpu_current_handle.Offset(count, m_descriptor_size);
+    m_gpu_current_handle.Offset(count, m_descriptor_size);
+    m_allocated += count;
+    return alloc;
+}
